7b75bb71f7d2efbb3d9f7050039c23885b4713eb
Ebb Counts <ecountsjr@gmail.com>
2024-11-16 23:42:56 -0600

    Potential Motor Driver Library

--- StepperMotorDriver.cpp
+++ StepperMotorDriver.cpp
@@ -77,6 +77,7 @@ void loop() {
 
 int main() {
     // Set initial desired position for testing
+    //the desired X and Y will come from the Neural Network
     desiredX = 10;
     desiredY = 10;
 
